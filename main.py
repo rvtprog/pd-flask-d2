b91c9015684b47aa58eaf89ce322afa2242a2e5b
marisdanne <maris.danne@rvt.lv>
2020-02-17 09:17:15 +0200

    new

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def getIndex():
   return "Can you redirect me to Home?"
+
+@app.route('/test')
+def testr():
+    return render_template('calc.html')
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", threaded=True, port=5050, debug=True) 
