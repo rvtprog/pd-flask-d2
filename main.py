57768571ba8a8aaabfdf4ad98601b95db1d81210
marisdanne <maris.danne@rvt.lv>
2020-02-17 09:51:22 +0200

    main ready

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def getIndex():
   return "Can you redirect me to Home?"
 
-@app.route('/test')
-def testr():
-    return render_template('calc.html')
-
-@app.route('/calc', methods = ['POST'])
-def calc():
-    return request.get_json()
-
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", threaded=True, port=5050, debug=True) 
+  app.run(threaded=True, port=5017, debug=True) 
